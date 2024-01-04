ebd7253f2c81ad51d03cfa26a5f4bfe4eb1c0cca
Shinsamago <149288990+Shinsamago@users.noreply.github.com>
2024-01-04 22:48:20 +0900

    Delete kadai_mdn_js/mdn.js

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-var dateName = new Date([2022, 8, 22]);
-
-var year = dateName.getFullYear();
-var month = dateName.getMonth()+1;
-var day = dateName.getDay();
-
-console.log( year + '年' + month + '月' + day + '日');
